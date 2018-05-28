6ab8a33ff04adf33a5a4684472baf623ed8a29c9
Marko Radovic <markoradovic996@gmail.com>
2018-05-28 23:15:12 +0200

    Trying to change sfscheduler to look like hci scheduler

--- HCI2/WpfApp/MainWindow.xaml.cs
+++ HCI2/WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Syncfusion.UI.Xaml.Schedule;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,44 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<MappedAppointment> MappedAppointments { get; set; }
+        public ObservableCollection<DateTime> VisibleDays { get; set; } 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MappedAppointments = new ObservableCollection<MappedAppointment>
+            {
+            new MappedAppointment{MappedSubject = "Meeting", MappedStartTime = DateTime.Now.Date.AddHours(10),
+                             MappedEndTime = DateTime.Now.Date.AddHours(11)},
+
+            new MappedAppointment{MappedSubject = "Conference", MappedStartTime = DateTime.Now.Date.AddHours(15),
+                             MappedEndTime = DateTime.Now.Date.AddHours(16)},
+            };
+
+            VisibleDays = new ObservableCollection<DateTime>
+            {
+                new DateTime(2018, 5, 28),
+                new DateTime(2018, 5, 29),
+                new DateTime(2018, 5, 30),
+                new DateTime(2018, 5, 31),
+                new DateTime(2018, 6, 1),
+                new DateTime(2018, 6, 2)
+            };
+            
+            this.DataContext = this;
+            //this.schedule.VisibleDatesChanging -= ;
         }
+
+    }
+
+    public class MappedAppointment
+    {
+        public string MappedSubject { get; set; }
+
+        public DateTime MappedStartTime { get; set; }
+
+        public DateTime MappedEndTime { get; set; }
     }
 }
---
+++ HCI2/WpfApp/View/MySfSchedule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Syncfusion.UI.Xaml.Schedule;
+
+namespace WpfApp.View
+{
+    public class MySfSchedule : SfSchedule
+    {
+        public MySfSchedule()
+        {
+            //this.PreviewMouseDown += new MouseButtonEventHandler(DisableNavigationClick);
+            //this.VisibleDatesChan;
+        }
+
+        private void DisableNavigationClick(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        public void MoveToNextView()
+        {
+            
+        }
+
+        public void MoveToPreviousView()
+        {
+
+        }
+
+        public new void MoveToDate(DateTime dateTime)
+        {
+             
+        }
+
+        public new void MoveToTime(TimeSpan ts)
+        {
+
+        }
+       
+    }
+}
